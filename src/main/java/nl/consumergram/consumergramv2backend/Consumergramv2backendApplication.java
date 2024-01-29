c8b10fe09696554313eab79f8d6af1f87e9aa1b2
rayzathegrave <rdgraaf213@gmail.com>
2024-01-29 15:01:00 +0100

    backend begonnen

---
+++ src/main/java/nl/consumergram/consumergramv2backend/Consumergramv2backendApplication.java
@@ -0,0 +1,13 @@
+package nl.consumergram.consumergramv2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumergramv2backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumergramv2backendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/consumergram/consumergramv2backend/Consumergramv2backendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.consumergram.consumergramv2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumergramv2backendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
